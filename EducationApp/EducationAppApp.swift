f10e2f77dc93a753934757716aedb008705112b2
isaac <isaac.kim@electrameccanica.com>
2023-03-27 16:50:23 -0700

    Initial Commit

---
+++ EducationApp/EducationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EducationAppApp.swift
+//  EducationApp
+//
+//  Created by Isaac Kim on 3/27/23.
+//
+
+import SwiftUI
+
+@main
+struct EducationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
