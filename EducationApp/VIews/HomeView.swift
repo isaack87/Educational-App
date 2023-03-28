2354b8776c9dd2e88a554de48eade1fea1f70fb6
isaac <isaac.kim@electrameccanica.com>
2023-03-27 21:41:59 -0700

    - Create Re-usable home screen cards - Render UI components on home screen

--- EducationApp/VIews/HomeView.swift
+++ EducationApp/VIews/HomeView.swift
@@ -12,18 +12,26 @@ struct HomeView: View {
     @EnvironmentObject var model: ContentModel
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+       
+        
+        ScrollView {
+            LazyVStack {
+                ForEach(model.modules) { module in
+                    //Learning Card
+                    HomeViewRow(image: module.content.image, title: "Learn \(module.category)", description: module.content.description, count: String(module.content.lessons.count), time: module.content.time)
+                    
+                    //Test Card
+                    HomeViewRow(image: module.test.image, title: "\(module.category) Test", description: module.test.description, count: String(module.content.lessons.count), time: module.test.time)
+                }
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
+            .environmentObject(ContentModel())
     }
 }
---
+++ EducationApp/VIews/HomeViewRow.swift
@@ -0,0 +1,78 @@
+//
+//  HomeViewRow.swift
+//  EducationApp
+//
+//  Created by Isaac Kim on 3/27/23.
+//
+
+import SwiftUI
+
+struct HomeViewRow: View {
+    
+    @EnvironmentObject var model: ContentModel
+    
+    var image: String
+    var title: String
+    var description: String
+    var count: String
+    var time: String
+    
+    var body: some View {
+        
+                    //Test Card
+        ZStack {
+            
+            Rectangle()
+                .foregroundColor(.white)
+                .cornerRadius(10)
+                .shadow(radius: 5)
+                .aspectRatio(CGSize(width: 335, height: 176), contentMode: .fit)
+            
+            
+            HStack {
+                
+                Image(image)
+                    .resizable()
+                    .frame(width:116, height:116)
+                    .clipShape(Circle())
+                
+                Spacer()
+                
+                VStack(alignment: .leading, spacing: 10) {
+                    
+                    Text(title)
+                        .bold()
+                    Text(description)
+                        .padding(.bottom, 20)
+                        .font(.caption)
+                    
+                    HStack {
+                        
+                        Image(systemName: "text.book.closed")
+                            .resizable()
+                            .frame(width:15, height:15)
+                        Text("\(count) lessons")
+                            .font(Font.system(size:10))
+                        
+                        Image(systemName: "clock")
+                            .resizable()
+                            .frame(width:15, height:15, alignment: .center)
+                        Text(time)
+                            .font(.caption)
+                        
+                    }
+                }
+                .padding(.leading, 20)
+                
+            }
+            .padding(.horizontal, 20)
+        }
+        
+    }
+}
+
+struct HomeViewRow_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeViewRow(image: "swift", title: "Learn Swift", description: "some description 2", count: "11", time: "3 hours")
+    }
+}
